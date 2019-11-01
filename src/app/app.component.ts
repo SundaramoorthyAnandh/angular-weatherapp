f5ccd442acb70a430d2c2a54955353e072f25ac5
SundaramoorthyAnandh <sundaramoorthy1224@gmail.com>
2019-11-01 15:43:59 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { DataService } from './api.data.service'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
   url: string = '';
